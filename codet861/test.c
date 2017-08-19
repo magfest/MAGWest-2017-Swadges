bb144567ca0fad1bcd55eca59ce8765f6b5a1a4e
Mark Murnane <mark@hackafe.net>
2017-08-19 01:36:34 -0400

    Added stupid button code and almost working touch sensing

--- codet861/test.c
+++ codet861/test.c
@@ -1,6 +1,8 @@
 //Copyright 2011 Charles Lohr (CC3.0 BY)
 //Based roughly off of http://www.youtube.com/watch?v=b71Vkq8dyf8 / http://www.artem.ru/cgi-bin/news?c=v&id=738
 
+#define ISP_TERM
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
@@ -17,6 +19,7 @@ void delay_ms(uint32_t time) {
 
 volatile unsigned char ThisCharToSend = 0;
 unsigned char rcnt;
+unsigned char buttons;
 
 ISR( USI_OVF_vect )
 {
@@ -73,23 +76,24 @@ static void setup_spi( void )
 	  B2: SPI Clock (Input)
 	  B3: LED	(Output) */
 	DDRB |= (1<<1);
-	DDRB &= 0xF2;
+	DDRB &= 0xF3;
 }
 
 void SetupHSTimer()
 {
 	TCCR0A = _BV(TCW0);
-	TCCR0B = (1<<CS10); //no prescaling
+	TCCR0B = (1<<CS01); //no prescaling
 
-//	TCCR1B |= (1<<CS12); //If uncommented, is /1024.
+	TCCR1B |= (1<<CS12); //If uncommented, is /1024.
 }
 
 
 void SetupSwitches()
 {
-//	DDRA  |= 0x8F;
-//	PORTA &= 0x70;
-	PORTA = 0x0;
+	DDRB  &= 0xB3;
+	PORTB |= 0x4C;
+	DDRA  &= 0xE0;
+	PORTA |= 0x1F;
 }
 
 volatile unsigned short TimerAtTrigger;
@@ -126,7 +130,7 @@ ISR( PCINT_vect, ISR_NAKED )
 #else
 ISR( PCINT_vect )
 {
-	uint16_t i = TCNT0L;
+	uint16_t i = wTCNT0L;
 	i |= ((unsigned int)TCNT0H << 8);
 
 	GIMSK &= ~_BV(PCIE0);     //Turn interrupt back off.
@@ -164,7 +168,7 @@ unsigned short TimeTest( unsigned char pin )
 
 
 #if 1
-		PORTA |= _BV(pin);
+//		PORTA |= _BV(pin);
 		while( !(PINA & _BV(pin)) );
 #else
 		set_sleep_mode(SLEEP_MODE_IDLE); // sleep mode is set here
@@ -226,120 +230,62 @@ ISR( TIMER1_OVF_vect )
 //	OCR1D+=8;
 }
 
+void read_buttons() {
+	buttons = (PINA & 0x1F) | ((PINB & _BV(2)) << 3) | ((PINB & _BV(3))<<3) | ((PINB & _BV(6))<<1);
+}
+
+float GetAngle(unsigned short max, unsigned short a, unsigned short b, float ref) {
+	unsigned short min, diff;
+	if (a>b) {
+		sendstr(" x ");
+		float m = b-a;
+		float n = max-a;
+		return ref + (((m)/(n))*60);
+	} else {
+		sendstr(" y ");
+		float m = a-b;
+		float n = max-b;
+		return ref - (((m)/(n))*60);
+	}
+	
+}
+
 int main()
 {
-	unsigned char i;
-	unsigned short s1, s2, s3, s4, s5;
-	
 	cli();
 	setup_clock();
 	SetupSwitches();
 	SetupHSTimer();   //TIMER0
+	#ifdef ISP_TERM
 	setup_spi();
+	#endif
 	sei();
 
-#if 1
-/*
-MCUCR &= ~_BV(PUD);
-PORTA = 0x1f;
-DDRA = ~0x1F;
-DDRB = 0x01;
-PORTB = _BV(2) | _BV(3) | _BV(6);
-*/
-while(1)
-{
-/*
-	uint8_t buttons = PINA | ((PINB & _BV(2)) << 3) | ((PINB & _BV(3))<<3) | ((PINB&_BV(6))<<1);
-	if ( buttons & 0x20 )
-	{
-		PORTB |= 3;
-	}
-	else
-	{
-		PORTB &=~3;
-	}
-	*/
-	uint8_t buttons = 0xaa;
-	sendhex2( buttons );
-	sendstr( "\n" );
-}
-
-#endif
-
-
-#if 0
-	//TCCR1A = _BV(TCW0);
-	//TCCR1B = (1<<CS10); //no prescaling
-
-	DDRB |= _BV(4) | _BV(5); //speaker.
-
-	TCCR1A = 0;
-	TCCR1B = _BV(CS10);
-	TCCR1C = _BV(PWM1D) | _BV(COM1D1);
-	TCCR1D = 0;
-	TCCR1E = 0;
-	OCR1C = 0xff;  //Set top
-	PLLCSR = _BV(PCKE) | _BV(PLLE); //Don't use PLL.
-	TIMSK |= _BV(TOIE1); //Enable overflow interrupt.
-while(1);
-while(1)
-{
-	_delay_us(600);
-	PORTB = 0x10;
-	_delay_us(600);
-	PORTB = 0x20;
-}
-while(1);
-#endif
-
-//Below here: Touch sensing.
-
-	//MCUCR |= _BV(PUD);
-
-	Biases[5] = TimeTest(5);
-		_delay_us(100);
-	Biases[6] = TimeTest(6);
-		_delay_us(100);
-	Biases[7] = TimeTest(7);
-		_delay_us(100);
-	_delay_ms(10);
-	Biases[5] = TimeTest(5);
-		_delay_us(100);
-	Biases[6] = TimeTest(6);
-		_delay_us(100);
-	Biases[7] = TimeTest(7);
-		_delay_us(100);
-
-/*
 	while(1)
 	{
-		int mark;
-		PORTA = 0;
-		DDRA &= ~_BV(7);
-		mark = 0;
-		while( !( PINA & _BV(7) ) ) mark++;
-		DDRA |= _BV(7);
-		sendhex4( mark );
-		sendstr( "\n: ");
-		_delay_ms(1);
+		unsigned short a,b,c;
+		a = GetTime(5);
+		b = GetTime(6);
+		c = GetTime(7);
+		float angle;
+		if (a>=b && a>=c) {
+			sendstr("a");
+			angle = GetAngle(a,b,c,60);
+		} else if (b>=c) {
+			sendstr("b");
+			angle = GetAngle(b,c,a,180);
+		} else {
+			sendstr("c");
+			angle = GetAngle(c,a,b,300);
+		}
+		sendhex2(a);
+		sendstr(" ");
+		sendhex2(b);
+		sendstr(" ");
+		sendhex2(c);
+		sendstr(" ");
+		sendhex4(round(angle));
+		sendstr("\n");
 	}
-*/
-	while(1)
-	{
-		_delay_us(100);
-		s1 = GetTime( 5 );
-		_delay_us(100);
-		s2 = GetTime( 6 );
-		_delay_us(100);
-		s3 = GetTime( 7 );
-		_delay_us(100);
-		sendhex2( s1 );
-		sendchr( ',' );
-		sendhex2( s2 );
-		sendchr( ',' );
-		sendhex2( s3 );
-		sendstr( "\n" );
-	}	
 }
-
 
